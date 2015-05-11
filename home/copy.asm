7a249b901a949b98f2da9fc46bddc484ad2b5a54
luckytyphlosion <alan.rj.huang@gmail.com>
2015-05-11 12:16:36 -0400

    Update copy.asm

--- home/copy.asm
+++ home/copy.asm
@@ -1,24 +1,34 @@
-FarCopyData::
+FarCopyData:: ; 009d (0:009d)
 ; Copy bc bytes from a:hl to de.
-	ld [wBuffer], a
+	ld [wd122+1], a
 	ld a, [H_LOADEDROMBANK]
 	push af
-	ld a, [wBuffer]
-	ld [H_LOADEDROMBANK], a
-	ld [MBC1RomBank], a
+	ld a, [wd122+1]
+	call BankswitchCommon
 	call CopyData
 	pop af
-	ld [H_LOADEDROMBANK], a
-	ld [MBC1RomBank], a
+	call BankswitchCommon
 	ret
 
-CopyData::
+CopyData:: ; 00b1 (0:00b1)
 ; Copy bc bytes from hl to de.
+	ld a,b
+	and a
+	jr z, .copybytes
+	ld a,c
+	and a ; is lower byte 0
+	jr z, .loop
+	inc b ; if not, increment b as there are <$100 bytes to copy
+.loop
+	call .copybytes
+	dec b
+	jr nz,.loop
+	ret
+	
+.copybytes	; 00c1
 	ld a, [hli]
 	ld [de], a
 	inc de
-	dec bc
-	ld a, c
-	or b
-	jr nz, CopyData
+	dec c
+	jr nz, .copybytes
 	ret
