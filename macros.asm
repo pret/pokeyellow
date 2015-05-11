c11fba8ea40505ee1ff56ce2e410c56324c1feb7
luckytyphlosion <alan.rj.huang@gmail.com>
2015-05-11 18:27:57 -0400

    Update macros.asm

--- macros.asm
+++ macros.asm
@@ -29,12 +29,10 @@ homecall: MACRO
 	ld a, [H_LOADEDROMBANK]
 	push af
 	ld a, BANK(\1)
-	ld [H_LOADEDROMBANK], a
-	ld [MBC1RomBank], a
+	call BankswitchCommon
 	call \1
 	pop af
-	ld [H_LOADEDROMBANK], a
-	ld [MBC1RomBank], a
+	jp BankswitchCommon
 	ENDM
 
 callba: MACRO
