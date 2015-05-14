fb327a0a949f9ce2f25116c29ee6c9d2aa83bd94
luckytyphlosion <alan.rj.huang@gmail.com>
2015-05-14 13:44:42 -0400

    Create main.asm

---
+++ yellow/bank3f/main.asm
@@ -0,0 +1,11 @@
+; asm to be added
+
+Func_fc5fa:: ; fc5fa (3f:45fa)
+
+Func_fc65b:: ; fc65b (3f:465b)
+
+Func_fc69a:: ; fc69a (3f:469a)
+
+Func_fcc08:: ; fcc08 (3f:4c08)
+
+Func_fcf0c:: ; fcf0c (3f:4f0c)
