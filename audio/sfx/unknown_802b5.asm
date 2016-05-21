d3d2e573a3b07075909b1680ef5e83a2429b0427
pikalaxalt <PikalaxALT@gmail.com>
2016-05-21 12:38:16 -0400

    Fix sfx separation

--- audio/sfx/unknown_802b5.asm
+++ audio/sfx/unknown_802b5.asm
@@ -6,16 +6,3 @@ SFX_802b5_4_Ch4:
 	unknownsfx0x20 0, 164, 96, 7
 	unknownsfx0x20 15, 193, 96, 7
 	endchannel
-
-SFX_802cc_4_Ch4:
-	duty $02
-	unknownsfx0x20 3, 180, 192, 7
-	unknownsfx0x20 0, 161, 128, 7
-	endchannel
-
-SFX_802d7_4_Ch7:
-	unknownnoise0x20 2, 241, 50
-	unknownnoise0x20 2, 0, 0
-	unknownnoise0x20 4, 230, 33
-	endchannel
-
---
+++ audio/sfx/unknown_802d7.asm
@@ -0,0 +1,6 @@
+SFX_802d7_4_Ch7:
+	unknownnoise0x20 2, 241, 50
+	unknownnoise0x20 2, 0, 0
+	unknownnoise0x20 4, 230, 33
+	endchannel
+
