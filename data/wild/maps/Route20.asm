82edc61dda97a17f95b884c385544deae8506af5
dannye <33dannye@gmail.com>
2021-03-25 18:05:18 -0500

    Fix Route20 wild mons tables

--- data/wild/maps/Route20.asm
+++ data/wild/maps/Route20.asm
@@ -1,15 +1,16 @@
 Route20Mons:
-	db $00
+	def_grass_wildmons 0 ; encounter rate
+	end_grass_wildmons
 
-	db $05
-	db 5,TENTACOOL
-	db 10,TENTACOOL
-	db 15,TENTACOOL
-	db 5,TENTACOOL
-	db 10,TENTACOOL
-	db 15,TENTACOOL
-	db 20,TENTACOOL
-	db 30,TENTACOOL
-	db 35,TENTACOOL
-	db 40,TENTACOOL
-
+	def_water_wildmons 5 ; encounter rate
+	db  5, TENTACOOL
+	db 10, TENTACOOL
+	db 15, TENTACOOL
+	db  5, TENTACOOL
+	db 10, TENTACOOL
+	db 15, TENTACOOL
+	db 20, TENTACOOL
+	db 30, TENTACOOL
+	db 35, TENTACOOL
+	db 40, TENTACOOL
+	end_water_wildmons
