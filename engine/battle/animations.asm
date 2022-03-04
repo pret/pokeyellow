be03e53465ac9be0ad3207851ea1707664c393cc
Rangi <remy.oukaour+rangi42@gmail.com>
2022-03-04 18:22:46 -0500

    Gen 1's Minimize graphic was not a complete 8x8 tile

--- engine/battle/animations.asm
+++ engine/battle/animations.asm
@@ -1790,7 +1790,7 @@ AnimationMinimizeMon:
 	ld bc, 7 * 7 * $10
 	call FillMemory
 	pop hl
-	ld de, $194
+	ld de, 7 * 3 * $10 + 4 * $10 + 4
 	add hl, de
 	ld de, MinimizedMonSprite
 	ld c, MinimizedMonSpriteEnd - MinimizedMonSprite
@@ -1806,7 +1806,15 @@ AnimationMinimizeMon:
 	jp AnimationShowMonPic
 
 MinimizedMonSprite:
-	INCBIN "gfx/battle/minimize.1bpp"
+; 8x5 partial tile graphic
+pusho
+opt b.X ; . = 0, X = 1
+	db %...XX...
+	db %..XXXX..
+	db %.XXXXXX.
+	db %..XXXX..
+	db %..X..X..
+popo
 MinimizedMonSpriteEnd:
 
 AnimationSlideMonDownAndHide:
