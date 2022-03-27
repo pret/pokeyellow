6472309b31344780bb926fb49d3bdfee45885a0f
vulcandth <vulcandth@gmail.com>
2022-03-26 20:08:48 -0500

    Finish template

--- vc/pokeyellow.constants.asm
+++ vc/pokeyellow.constants.asm
@@ -26,3 +26,44 @@ YellowIntroScene15_Index = $f
 	vc_const "S"
 	vc_const "F"
 	vc_const MEGA_PUNCH
+
+; [FPA 002 Begin]
+	vc_const "U"
+	vc_const "I"
+	vc_const GUILLOTINE
+
+; [FPA 003 Begin]
+	vc_const "K"
+	vc_const MEGA_KICK
+
+; [FPA 004 Begin]
+	vc_const "B"
+	vc_const "Z"
+	vc_const BLIZZARD
+
+; [FPA 005 Begin]
+	vc_const BUBBLEBEAM
+
+; [FPA 006 Begin]
+	vc_const "H"
+	vc_const "Y"
+	vc_const HYPER_BEAM
+
+; [FPA 007 Begin]
+	vc_const "T"
+	vc_const "N"
+	vc_const THUNDERBOLT
+
+; [FPA 008 Begin]
+	vc_const HAZE
+
+; [FPA 009 Begin]
+	vc_const "R"
+	vc_const "F"
+	vc_const REFLECT
+
+; [FPA 010 Begin]
+	vc_const SELFDESTRUCT
+
+; [FPA 011 Begin]
+	vc_const EXPLOSION
