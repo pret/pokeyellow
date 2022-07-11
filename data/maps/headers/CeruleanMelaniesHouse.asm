0397c8baa623025572429cacb3cffc01ba87b859
vulcandth <vulcandth@gmail.com>
2022-07-10 21:10:18 -0500

    Remove empty lines on the top of map header files

--- data/maps/headers/CeruleanMelaniesHouse.asm
+++ data/maps/headers/CeruleanMelaniesHouse.asm
@@ -1,3 +1,2 @@
-
 	map_header CeruleanMelaniesHouse, CERULEAN_MELANIES_HOUSE, HOUSE, 0
 	end_map_header
--- data/maps/headers/SummerBeachHouse.asm
+++ data/maps/headers/SummerBeachHouse.asm
@@ -1,3 +1,2 @@
-
 	map_header SummerBeachHouse, SUMMER_BEACH_HOUSE, BEACH_HOUSE, 0
 	end_map_header
