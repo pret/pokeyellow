6fbdac0297d34f60eb5ecced2731c4d5fb97fd86
PokefanMarcel <166040321+PokefanMarcel@users.noreply.github.com>
2026-06-18 22:52:44 +0200

    Use OAM constants

--- data/sprite_anims/surfing_pikachu_oam.asm
+++ data/sprite_anims/surfing_pikachu_oam.asm
@@ -38,7 +38,7 @@ SurfingPikachuOAMData:
 
 .Frame0:
 	db 1
-	db $fc, $fc, $00, $00
+	db $fc, $fc, $00, OAM_PAL0
 
 .Frame1:
 .Frame2:
@@ -59,124 +59,124 @@ SurfingPikachuOAMData:
 .Frame17:
 .Frame18:
 	db 9
-	db $f4, $f4, $00, $00
-	db $f4, $fc, $01, $00
-	db $f4, $04, $02, $00
-	db $fc, $f4, $10, $00
-	db $fc, $fc, $11, $00
-	db $fc, $04, $12, $00
-	db $04, $f4, $20, $00
-	db $04, $fc, $21, $00
-	db $04, $04, $22, $00
+	db $f4, $f4, $00, OAM_PAL0
+	db $f4, $fc, $01, OAM_PAL0
+	db $f4, $04, $02, OAM_PAL0
+	db $fc, $f4, $10, OAM_PAL0
+	db $fc, $fc, $11, OAM_PAL0
+	db $fc, $04, $12, OAM_PAL0
+	db $04, $f4, $20, OAM_PAL0
+	db $04, $fc, $21, OAM_PAL0
+	db $04, $04, $22, OAM_PAL0
 
 .Frame22:
 .Frame23:
 .Frame24:
 	db 12
-	db $f8, $e8, $00, $00
-	db $f8, $f0, $01, $00
-	db $f8, $f8, $02, $00
-	db $f8, $00, $03, $00
-	db $f8, $08, $04, $00
-	db $f8, $10, $05, $00
-	db $00, $e8, $10, $00
-	db $00, $f0, $11, $00
-	db $00, $f8, $12, $00
-	db $00, $00, $13, $00
-	db $00, $08, $14, $00
-	db $00, $10, $15, $00
+	db $f8, $e8, $00, OAM_PAL0
+	db $f8, $f0, $01, OAM_PAL0
+	db $f8, $f8, $02, OAM_PAL0
+	db $f8, $00, $03, OAM_PAL0
+	db $f8, $08, $04, OAM_PAL0
+	db $f8, $10, $05, OAM_PAL0
+	db $00, $e8, $10, OAM_PAL0
+	db $00, $f0, $11, OAM_PAL0
+	db $00, $f8, $12, OAM_PAL0
+	db $00, $00, $13, OAM_PAL0
+	db $00, $08, $14, OAM_PAL0
+	db $00, $10, $15, OAM_PAL0
 
 .Frame25:
 	db 3
-	db $fc, $0b, $00, $10
-	db $04, $03, $0f, $10
-	db $04, $0b, $10, $10
+	db $fc, $0b, $00, OAM_PAL1
+	db $04, $03, $0f, OAM_PAL1
+	db $04, $0b, $10, OAM_PAL1
 
 .Frame19:
 	db 6
-	db $fc, $f0, $00, $30
-	db $fc, $08, $00, $10
-	db $04, $f0, $10, $30
-	db $04, $f8, $0f, $30
-	db $04, $00, $0f, $10
-	db $04, $08, $10, $10
+	db $fc, $f0, $00, OAM_PAL1 | OAM_XFLIP
+	db $fc, $08, $00, OAM_PAL1
+	db $04, $f0, $10, OAM_PAL1 | OAM_XFLIP
+	db $04, $f8, $0f, OAM_PAL1 | OAM_XFLIP
+	db $04, $00, $0f, OAM_PAL1
+	db $04, $08, $10, OAM_PAL1
 
 .Frame20:
 	db 12
-	db $f4, $f0, $00, $10
-	db $f4, $f8, $01, $10
-	db $f4, $00, $01, $30
-	db $f4, $08, $00, $30
-	db $fc, $f0, $10, $10
-	db $fc, $f8, $11, $10
-	db $fc, $00, $11, $30
-	db $fc, $08, $10, $30
-	db $04, $f0, $20, $10
-	db $04, $f8, $21, $10
-	db $04, $00, $21, $30
-	db $04, $08, $20, $30
+	db $f4, $f0, $00, OAM_PAL1
+	db $f4, $f8, $01, OAM_PAL1
+	db $f4, $00, $01, OAM_PAL1 | OAM_XFLIP
+	db $f4, $08, $00, OAM_PAL1 | OAM_XFLIP
+	db $fc, $f0, $10, OAM_PAL1
+	db $fc, $f8, $11, OAM_PAL1
+	db $fc, $00, $11, OAM_PAL1 | OAM_XFLIP
+	db $fc, $08, $10, OAM_PAL1 | OAM_XFLIP
+	db $04, $f0, $20, OAM_PAL1
+	db $04, $f8, $21, OAM_PAL1
+	db $04, $00, $21, OAM_PAL1 | OAM_XFLIP
+	db $04, $08, $20, OAM_PAL1 | OAM_XFLIP
 
 .Frame21:
 	db 3
-	db $04, $f4, $00, $00
-	db $04, $fc, $01, $00
-	db $04, $04, $02, $00
+	db $04, $f4, $00, OAM_PAL0
+	db $04, $fc, $01, OAM_PAL0
+	db $04, $04, $02, OAM_PAL0
 
 .Frame26:
 	db 3
-	db $fc, $f4, $bf, $00
-	db $fc, $fc, $d5, $00
-	db $fc, $04, $d0, $00
+	db $fc, $f4, $bf, OAM_PAL0
+	db $fc, $fc, $d5, OAM_PAL0
+	db $fc, $04, $d0, OAM_PAL0
 
 .Frame27:
 	db 4
-	db $fc, $f0, $bf, $00
-	db $fc, $f8, $d1, $00
-	db $fc, $00, $d5, $00
-	db $fc, $08, $d0, $00
+	db $fc, $f0, $bf, OAM_PAL0
+	db $fc, $f8, $d1, OAM_PAL0
+	db $fc, $00, $d5, OAM_PAL0
+	db $fc, $08, $d0, OAM_PAL0
 
 .Frame28:
 	db 4
-	db $fc, $f0, $bf, $00
-	db $fc, $f8, $d3, $00
-	db $fc, $00, $d5, $00
-	db $fc, $08, $d0, $00
+	db $fc, $f0, $bf, OAM_PAL0
+	db $fc, $f8, $d3, OAM_PAL0
+	db $fc, $00, $d5, OAM_PAL0
+	db $fc, $08, $d0, OAM_PAL0
 
 .Frame29:
 	db 4
-	db $fc, $f0, $bf, $00
-	db $fc, $f8, $d7, $00
-	db $fc, $00, $d5, $00
-	db $fc, $08, $d0, $00
+	db $fc, $f0, $bf, OAM_PAL0
+	db $fc, $f8, $d7, OAM_PAL0
+	db $fc, $00, $d5, OAM_PAL0
+	db $fc, $08, $d0, OAM_PAL0
 
 .Frame30:
 	db 4
-	db $fc, $f0, $bf, $00
-	db $fc, $f8, $d1, $00
-	db $fc, $00, $d8, $00
-	db $fc, $08, $d0, $00
+	db $fc, $f0, $bf, OAM_PAL0
+	db $fc, $f8, $d1, OAM_PAL0
+	db $fc, $00, $d8, OAM_PAL0
+	db $fc, $08, $d0, OAM_PAL0
 
 .Frame31:
 	db 4
-	db $fc, $f0, $bf, $00
-	db $fc, $f8, $d5, $00
-	db $fc, $00, $d0, $00
-	db $fc, $08, $d0, $00
+	db $fc, $f0, $bf, OAM_PAL0
+	db $fc, $f8, $d5, OAM_PAL0
+	db $fc, $00, $d0, OAM_PAL0
+	db $fc, $08, $d0, OAM_PAL0
 
 .Frame32:
 .Frame33:
 .Frame34:
 .Frame35:
 	db 12
-	db $f4, $f0, $03, $20
-	db $f4, $f8, $02, $20
-	db $f4, $00, $01, $20
-	db $f4, $08, $00, $20
-	db $fc, $f0, $13, $20
-	db $fc, $f8, $12, $20
-	db $fc, $00, $11, $20
-	db $fc, $08, $10, $20
-	db $04, $f0, $23, $20
-	db $04, $f8, $22, $20
-	db $04, $00, $21, $20
-	db $04, $08, $20, $20
+	db $f4, $f0, $03, OAM_XFLIP
+	db $f4, $f8, $02, OAM_XFLIP
+	db $f4, $00, $01, OAM_XFLIP
+	db $f4, $08, $00, OAM_XFLIP
+	db $fc, $f0, $13, OAM_XFLIP
+	db $fc, $f8, $12, OAM_XFLIP
+	db $fc, $00, $11, OAM_XFLIP
+	db $fc, $08, $10, OAM_XFLIP
+	db $04, $f0, $23, OAM_XFLIP
+	db $04, $f8, $22, OAM_XFLIP
+	db $04, $00, $21, OAM_XFLIP
+	db $04, $08, $20, OAM_XFLIP
